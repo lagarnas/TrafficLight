bf56b82e65a8badf0a812083938d16cf23ee5e06
Leonteva Anastasia <lagarnas@gmail.com>
2020-02-17 15:42:46 +0400

    Initial Commit

---
+++ trafficLight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  trafficLight
+//
+//  Created by Анастасия Лагарникова on 17.02.2020.
+//  Copyright © 2020 lagarnas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
